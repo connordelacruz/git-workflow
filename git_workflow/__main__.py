fbcd54549683fcc6010d88736f47a1db5f57b09e
Connor de la Cruz <connor.c.delacruz@gmail.com>
2020-10-20 14:54:55 -0500

    Added entrypoint

---
+++ git_workflow/__main__.py
@@ -0,0 +1,7 @@
+# TODO DOC, implement
+
+def main():
+    print('TESTING')
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -9,6 +9,14 @@ setup(
     author_email='connor.c.delacruz@gmail.com',
     # license='MIT',
     packages=['git_workflow'],
-    install_requires=['GitPython>=3.1,<3.2'],
+    entry_points={
+        'console_scripts': [
+            'workflow = git_workflow.__main__:main',
+        ]
+    },
+    install_requires=[
+        'GitPython>=3.1,<3.2',
+        'blessings>=1.7,<1.8',
+    ],
 )
 
