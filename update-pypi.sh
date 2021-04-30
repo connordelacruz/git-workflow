0e6986c175549c84848e0ee51b1f0bc5ef7ea461
Connor de la Cruz <connor.c.delacruz@gmail.com>
2021-04-30 14:49:21 -0500

    Bumped version, updated TODO list, added -t arg to update-pypi.sh

--- update-pypi.sh
+++ update-pypi.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 set -o errexit
+arg_test=
+while getopts 't' opt; do
+    case ${opt} in
+        t)
+            arg_test=1
+            ;;
+    esac
+done
+shift $((OPTIND -1))
 # Build packages
 python -m build
-# TODO args for testpypi
-# twine upload --repository testpypi dist/*
-twine upload dist/*
+# Upload
+if [[ -z "$arg_test" ]]; then
+    twine upload dist/*
+else
+    twine upload --repository testpypi dist/*
+fi
 # Cleanup
 rm dist/*
 
